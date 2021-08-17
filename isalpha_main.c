50f09f6149f05e72ae04050aa33cb5c3afcc8a2e
Moacir Amaro <moacir@gmail.com>
2021-08-17 19:22:07 -0300

    Done

--- isalpha_main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int ft_isalpha(int c);
-
-int main(void)
-{
-    char c = '1';
-    printf("C = %c\nretorna no isalpha: %d\n", c, ft_isalpha(c));
-}
--- isdigit_main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int ft_isdigit(int c);
-
-int main(void)
-{
-    char c = 'a';
-    printf("C = %c\nretorna no ft_isdigit: %d\n", c, ft_isdigit(c));
-}
