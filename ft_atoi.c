156902d8f1f61bdb95ecb0c0e5db5f865ee847b4
Moacir Amaro <moacir@gmail.com>
2021-09-02 20:42:43 -0300

    Normalize atoi error

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,16 +6,17 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 17:22:16 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/01 11:51:53 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/02 20:42:24 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+static int	ft_count_nums(char *str);
 
 int	ft_atoi(char *str)
 {
-	unsigned int	num;
-	int				sign;
+	size_t	num;
+	int		sign;
 
 	sign = 1;
 	while ((9 <= *str && *str <= 15) || *str == ' ')
@@ -26,6 +27,8 @@ int	ft_atoi(char *str)
 			sign = -1;
 		*str++;
 	}
+	if (ft_count_nums(str) > 12)
+		return (-1);
 	num = 0;
 	while ('9' >= *str && *str >= '0')
 	{
@@ -34,4 +37,19 @@ int	ft_atoi(char *str)
 		++str;
 	}
 	return (num);
+}
+
+static int	ft_count_nums(char *str)
+{
+	char	**tmp;
+	size_t	count;
+
+	tmp = &str;
+	count = 0;
+	while ('9' >= *(*tmp) && *(*tmp) >= '0')
+	{
+		count++;
+		++*tmp;
+	}
+	return (count);
 }
