243293169f0b1760b5d95d7efd5f95b0cff141fa
Moacir Amaro <moacir@gmail.com>
2021-09-06 14:51:55 -0300

    Fix ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 15:37:33 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/06 14:51:07 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/06 14:51:53 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*ptr;
 	size_t	i;
 
-	if(start > ft_strlen(s))
+	if (start > ft_strlen(s))
 		return (0);
 	ptr = malloc(len);
 	if (ptr == NULL)
