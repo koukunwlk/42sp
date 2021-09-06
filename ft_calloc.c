1813cc14bee4a1385209769023461e4e0c01387e
Moacir Amaro <moacir@gmail.com>
2021-09-06 14:26:17 -0300

    Fix ft_calloc bug with 0 value

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 21:37:35 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/06 14:22:15 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/06 14:26:11 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
-	if (nmemb == 0 || size == 0 || ptr == 0)
-		return (0);
 	ptr = malloc(nmemb * size);
 	if(ptr == NULL)
 		return (0);
