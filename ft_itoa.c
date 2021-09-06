01bd305c4eecc9d50ab5eb321c749f8ea2b27671
Moacir Amaro <moacir@gmail.com>
2021-09-06 20:09:00 -0300

    fix ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 19:21:43 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/06 20:05:32 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/06 20:08:57 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,15 +51,17 @@ static char	*int_converter(int i, int n)
 
 	j = 0;
 	tmp = ft_calloc(sizeof(char *), i + 1);
-	if(tmp == NULL)
-		return (NULL);	
 	if (n < 0)
 	{
+		free(tmp)
+		tmp = ft_calloc(sizeof(char *), i + 2);
 		holder = n * -1;
-		tmp[j] = '-';
+		tmp[j++] = '-';
 	}
 	else
 		holder = n;
+	if(tmp == NULL)
+		return (NULL);	
 	div = 1;
 	while (--i)
 		div *= 10;
