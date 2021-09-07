a5239678ef6c2f5396a5c4a3afe14da738b2bfc3
Moacir Amaro <moacir@gmail.com>
2021-09-06 21:40:59 -0300

    fix ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,92 +1,53 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/26 19:21:43 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/06 21:32:15 by mamaro-d         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-static int	count_num(int n);
-static char	*int_converter(int i, int n);
+static char	*ft_array(char *x, unsigned int number, long int len)
+{
+	while (number > 0)
+	{
+		x[len--] = 48 + (number % 10);
+		number = number / 10;
+	}
+	return (x);
+}
+
+static long int	ft_len(int n)
+{
+	int					len;
+
+	len = 0;
+	if (n <= 0)
+		len = 1;
+	while (n != 0)
+	{
+		len++;
+		n = n / 10;
+	}
+	return (len);
+}
 
 char	*ft_itoa(int n)
 {
-	char	*tmp;
-	size_t	i;
+	char				*x;
+	long int			len;
+	unsigned int		number;
+	int					sign;
 
+	sign = 1;
+	len = ft_len(n);
+	x = (char *)malloc(sizeof(char) * (len + 1));
+	if (!(x))
+		return (NULL);
+	x[len--] = '\0';
 	if (n == 0)
-	{
-		tmp = (char *)ft_calloc(1, 2);
-			if (tmp == NULL)
-				return (NULL);
-		*tmp = 48;
-		return (tmp);
-	}
-	i = count_num(n);
-	tmp = int_converter(i, n);
-	return (tmp);
-}
-
-static void	fill_tmp(unsigned int holder, char *tmp, int div, int j)
-{
-	while (div)
-	{	
-		tmp[j] = (holder / div) + 48;
-		holder %= div;
-		div /= 10;
-		j++;
-	}
-	tmp[j] = '\0';
-}
-
-static char	*int_converter(int i, int n)
-{
-	char			*tmp;
-	int				div;
-	unsigned int	holder;
-	int				j;
-
-	j = 0;
-	if (n < 0)
-		tmp = (char *)ft_calloc(sizeof(char), i + 2);
-	tmp = (char *)ft_calloc(sizeof(char), i + 1);
-	if(tmp == NULL)
-		return (NULL);	
+		x[0] = '0';
 	if (n < 0)
 	{
-		holder = n * -1;
-		tmp[j] = '-';
-		j++;
+		sign *= -1;
+		number = n * -1;
+		x[0] = '-';
 	}
 	else
-		holder = n;
-	div = 1;
-	while (--i)
-		div *= 10;
-	fill_tmp(holder, tmp, div, j);
-	return (tmp);
-}
-
-static int	count_num(int n)
-{
-	size_t			i;
-	unsigned int	tmp_num;
-
-	i = 0;
-	if (n < 0)
-		tmp_num = n * -1;
-	else
-		tmp_num = n;
-	while (tmp_num)
-	{
-		tmp_num /= 10;
-		i++;
-	}
-	return (i);
+		number = n;
+	x = ft_array(x, number, len);
+	return (x);
 }
