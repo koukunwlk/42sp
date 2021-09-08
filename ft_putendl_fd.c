3e9feaee357e22691c278368bfd8d688ffd18420
Moacir Amaro <moacir@gmail.com>
2021-09-08 15:06:31 -0300

    Protect ft_putendl_fd

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 15:28:17 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/08/27 15:29:37 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/08 15:06:22 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_putendl_fd(char *s, int fd)
 {
 	size_t	i;
 
+	if (!s || !fd)
+		return ;
 	i = 0;
 	while (s[i])
 	{
