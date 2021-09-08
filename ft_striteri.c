13d673ebaf7f6390b797c6a1d2916f7da76eef45
Moacir Amaro <moacir@gmail.com>
2021-09-08 15:10:26 -0300

    Protect ft_striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 14:32:40 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/08/27 14:45:12 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/08 15:10:16 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
 
+	if (!s || !f)
+		return ;
 	i = 0;
 	while (s[i])
 	{
