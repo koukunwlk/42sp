e5aa21c8578ab921c003fa20b4e4624799240648
Moacir Amaro <moacir@gmail.com>
2021-09-09 16:25:12 -0300

    fix strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,20 +6,37 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 16:42:38 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/09 15:17:55 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/09 16:22:50 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *s1, const char *s2, size_t n)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	int		needle_len;
+
+	if(needle[0] == '\0')
+		return ((char *)haystack);
+	needle_len = ft_strlen(needle);
+	while (*haystack && len)
+	{
+		if(ft_strncmp(haystack, needle, needle_len))
+			return ((char *)haystack);
+		haystack++;
+		len--;
+	}
+	return (NULL);
+}
+/* {
 	size_t	i;
 	size_t	j;
+	int		
 
 	i = 0;
 	if(s2[0] == '\0')
 		return ((char *)s1);
+	
 	while (s1[i] != 0 && n)
 	{
 		j = 0;
@@ -35,3 +52,4 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	}
 	return (0);
 }
+ */
