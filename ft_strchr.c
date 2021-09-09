7ae7530bbb9a160ed9aeec8f707911d433891348
Moacir Amaro <moacir@gmail.com>
2021-09-09 14:37:01 -0300

    fix strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 11:42:10 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/09 14:34:24 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/09 14:36:38 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 char	*ft_strchr(const char *s, int c)
 {
 	size_t	i;
+	char	*ptr;
 
+	ptr = s;
 	i = 0;
 	while (i++ <= ft_strlen(s))
 	{
-		if (s[i] == c)
-			return ((char *)s[i]);
+		if (ptr[i] == c)
+			return (ptr[i]);
 	}
 	return (NULL);
 }
