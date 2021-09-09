1ececab4cc3078d1825d4341ef68cc5d64e06df1
Moacir Amaro <moacir@gmail.com>
2021-09-09 15:13:33 -0300

    fix strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 16:42:38 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/09 15:08:10 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/09 15:13:31 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	while (s1[i] != 0 && n)
 	{
 		j = 0;
-		while (s1[i] == s2[j])
+		while (s1[i] == s2[j] && s2[j] != '\0')
 		{
 			i++;
 			j++;
