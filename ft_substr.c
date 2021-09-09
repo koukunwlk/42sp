035e14ae19661506679370a3fb9469af7e12a0c5
Moacir Amaro <moacir@gmail.com>
2021-09-09 20:19:03 -0300

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,20 +6,44 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 15:37:33 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/09 20:13:47 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/09 20:19:00 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char
+	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	str = (char*)malloc(sizeof(*s) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = 0;
+	return (str);
+}
+/* char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
 	size_t	i;
 
 	if (s  == '\0')
 		return (NULL);
-	ptr = (char *)ft_calloc(sizeof(char), len + 1);
+	ptr = ft_calloc(sizeof(char), len);
 	if (ptr == NULL)
 		return (NULL);
 	i = 0;
@@ -33,3 +57,4 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	return (ptr);
 }
+ */
