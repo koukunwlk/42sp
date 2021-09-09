e4a4d99dff16d9407d7d9014378fd6fe7bc80e88
Moacir Amaro <moacir@gmail.com>
2021-09-09 17:56:39 -0300

    fix strnstr

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,53 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 16:26:40 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/09 16:53:09 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/09 17:56:35 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(const char *s1, char const *set)
+#include "libft.h"
+
+static int
+	ft_char_in_set(char c, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char
+	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	size_t	i;
+	size_t	start;
+	size_t	end;
+
+	start = 0;
+	while (s1[start] && ft_char_in_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_char_in_set(s1[end - 1], set))
+		end--;
+	str = (char*)malloc(sizeof(*s1) * (end - start + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		str[i++] = s1[start++];
+	str[i] = 0;
+	return (str);
+}
+/* char	*ft_strtrim(const char *s1, char const *set)
 {
 	size_t		size_s;
 	char		*ptr;
@@ -30,4 +70,4 @@ char	*ft_strtrim(const char *s1, char const *set)
 		size_s--;
 	ptr = ft_substr((char *)s1, 0, size_s + 1);
 	return (ptr);
-}
+} */
