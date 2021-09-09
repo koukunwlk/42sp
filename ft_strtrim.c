81f74ea357e369b7d80493d70b54491473f2a262
Moacir Amaro <moacir@gmail.com>
2021-09-09 19:20:37 -0300

    fix strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,12 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 16:26:40 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/09 19:14:49 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/09 19:20:31 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-#include "libft.h"
+#include <stdio.h>
 
 char	*ft_strtrim(const char *s1, char const *set)
 {
@@ -27,9 +26,10 @@ char	*ft_strtrim(const char *s1, char const *set)
 	end = ft_strlen(s1);
 	while (end && ft_strchr(set, s1[end]))
 		end--;
-	end++;
 	len = ft_strlen(s1) - end;
 	ptr = ft_calloc(1, len);
-	ptr = ft_substr((char *)s1, 0, end);
+	if (ptr == NULL)
+		return(NULL);
+	ptr = ft_substr((char *)s1, 0, end + 1);
 	return (ptr);
 }
