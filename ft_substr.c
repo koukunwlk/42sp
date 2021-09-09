293084d9e42af4d9f2819fac5034bea77c4699ac
Moacir Amaro <moacir@gmail.com>
2021-09-09 20:21:57 -0300

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,39 +6,13 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 15:37:33 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/09 20:20:14 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/09 20:21:54 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char			*str;
-	unsigned int	i;
-	unsigned int	j;
-	unsigned int	slen;
-
-	if (!s)
-		return (NULL);
-	slen = ft_strlen(s);
-	if (!(str = malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	i = start;
-	j = 0;
-	if (start < slen)
-	{
-		while (i < start + len && s[i] != '\0')
-		{
-			str[j] = s[i];
-			j++;
-			i++;
-		}
-	}
-	str[j] = '\0';
-	return (str);
-}
-/* char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
 	size_t	i;
@@ -59,4 +33,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	return (ptr);
 }
- */
