ad7709d387014aa47007eded0c2cb9e4c792b3dd
Moacir Amaro <moacir@gmail.com>
2021-09-09 19:08:13 -0300

    fix strnstr

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 16:26:40 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/09 17:56:35 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/09 18:04:21 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,60 +14,18 @@
 
 #include "libft.h"
 
-static int
-	ft_char_in_set(char c, char const *set)
-{
-	size_t	i;
-
-	i = 0;
-	while (set[i])
-	{
-		if (set[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-char
-	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*str;
-	size_t	i;
-	size_t	start;
-	size_t	end;
-
-	start = 0;
-	while (s1[start] && ft_char_in_set(s1[start], set))
-		start++;
-	end = ft_strlen(s1);
-	while (end > start && ft_char_in_set(s1[end - 1], set))
-		end--;
-	str = (char*)malloc(sizeof(*s1) * (end - start + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (start < end)
-		str[i++] = s1[start++];
-	str[i] = 0;
-	return (str);
-}
-/* char	*ft_strtrim(const char *s1, char const *set)
+char	*ft_strtrim(const char *s1, char const *set)
 {
 	size_t		size_s;
 	char		*ptr;
 
 	if (!s1 || !set)
 		return (NULL);
-	if(*s1 == 32 || (*s1 >= 9 && *s1 <= 13))
-		s1++;
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	size_s = ft_strlen(s1);
-	if(s1[size_s] == 32 || (s1[size_s] >= 9 && s1[size_s] <= 13))
-		size_s--;
 	while (size_s && ft_strchr(set, s1[size_s]))
 		size_s--;
 	ptr = ft_substr((char *)s1, 0, size_s + 1);
 	return (ptr);
-} */
+}
