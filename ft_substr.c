ba2bc6857feea409230e919ccb58b9cad791d898
Moacir Amaro <moacir@gmail.com>
2021-09-13 13:52:22 -0300

    final check

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 15:37:33 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/13 13:40:43 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/13 13:52:19 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (s == 0)
 		return (NULL);
-	ptr = (char *)malloc(sizeof(char) * (len + 1));
+	ptr = (char *)ft_calloc(sizeof(char) * (len + 1));
 	if (ptr == NULL)
 		return (NULL);
 	i = 0;
@@ -31,6 +31,5 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 			i++;
 		}
 	}
-	ptr[i] = '\0';
 	return (ptr);
 }
