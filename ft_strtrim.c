0bf9d414c49a6ee933d897b74eb447a72d279a60
Moacir Amaro <moacir@gmail.com>
2021-09-13 16:40:53 -0300

    final check

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 16:26:40 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/13 16:36:43 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/13 16:40:48 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,7 @@ char	*ft_strtrim(const char *s1, char const *set)
 	end = ft_strlen(s1);
 	while (end > ini && ft_find_char(set, s1[end - 1]))
 		end--;
-	end++;
-	ptr = malloc(sizeof(char)* (end - ini));
+	ptr = ft_calloc(1, end - ini);
 	if (ptr == NULL)
 		return (NULL);
 	i = 0;
@@ -38,7 +37,6 @@ char	*ft_strtrim(const char *s1, char const *set)
 		ptr[i] = s1[ini + i];
 		i++;
 	}
-	ptr[i] = 0;
 	return (ptr);
 }
 
