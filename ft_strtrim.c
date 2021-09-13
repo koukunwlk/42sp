f2765811ebd150570d627393769499b8e362141a
Moacir Amaro <moacir@gmail.com>
2021-09-13 17:35:18 -0300

    fix strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 16:26:40 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/13 16:40:48 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/13 17:34:39 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ char	*ft_strtrim(const char *s1, char const *set)
 	end = ft_strlen(s1);
 	while (end > ini && ft_find_char(set, s1[end - 1]))
 		end--;
-	ptr = ft_calloc(1, end - ini);
+	ptr = (char *)ft_calloc(1, end - ini + 1);
 	if (ptr == NULL)
 		return (NULL);
 	i = 0;
