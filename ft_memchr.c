675ef6aad48b054e690e8996a70bce978f29138e
Moacir Amaro <moacir@gmail.com>
2021-09-13 16:23:01 -0300

    final check

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 15:32:35 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/13 16:20:54 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/13 16:22:51 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (n--)
 	{
 		if (*pointer == (unsigned char)c)
-			return (pointer);
+			return ((void *)pointer);
 		pointer++;
 	}
 	return (NULL);
