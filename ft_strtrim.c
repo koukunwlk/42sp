736750ed2693416a5b65b2d21ecfcea9fe4421e9
Moacir Amaro <moacir@gmail.com>
2021-09-13 16:33:54 -0300

    final check

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 16:26:40 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/13 16:32:53 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/13 16:33:43 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ char	*ft_strtrim(const char *s1, char const *set)
 		ptr[i] = s1[ini + i];
 		i++;
 	}
-	ptr[i] = 0
+	ptr[i] = 0;
 	return (ptr);
 }
 
