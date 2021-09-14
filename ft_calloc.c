9fee5b810c99849a679ab73d72b84cfc2d97f6de
Moacir Amaro <moacir@gmail.com>
2021-09-14 18:39:33 -0300

    fix strtrim

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,19 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 21:37:35 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/13 12:49:25 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/14 18:38:38 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
-	ptr = malloc(count * size);
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	ft_bzero((char *)ptr, size * count);
+	ft_bzero((char *)ptr, size * nmemb);
 	return (ptr);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mamaro-d <mamaro-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 15:37:33 by mamaro-d          #+#    #+#             */
-/*   Updated: 2021/09/14 18:36:07 by mamaro-d         ###   ########.fr       */
+/*   Updated: 2021/09/14 18:39:11 by mamaro-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if ((size_t)ft_strlen(s) < start)
 		return (ft_strdup(""));
-	ptr = ft_calloc(len + 1, sizeof(char));
+	ptr = (char *)ft_calloc(len + 1, sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 	else
